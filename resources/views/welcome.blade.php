835a45c4cd314958913fa5e03a5c246c558e0403
edoardo <edoardo.collo@gmail.com>
2021-04-08 15:43:26 +0200

    init + nav init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+      <nav class="container-fluid d-flex justify-content-center ">
+        <img src="img/logo.png" alt="" class="">
+        <div class="container m-0 d-flex flex-wrap justify-content-flex-start align-items-center">
+          <span class="col-md-6">TRUST loremsdsfsdfddfs</span>
+          <ul class="d-flex col-md-6 justify-content-center list-group-orizontal">
+            <li class="list-group-item">lorem</li>
+            <li class="list-group-item">lorem</li>
+            <li class="list-group-item">lorem</li>
+            <li class="list-group-item">lorem</li>
+          </ul>
+          <div class=" col-md-6">
+            <input type="text" name="" value="">
+          </div>
+          <div class="col-md-6 d-flex">
+            <div class="col-md-4">
+                lorem_card
+            </div>
+            <div class="col-md-4">
+                lorem_card
+            </div>
+            <div class="col-md-4">
+                lorem_card
+            </div>
+          </div>
+        </div>
+        <div class="">
+          <h2>CART</h2>
+        </div>
+      </nav>
+    </body>
+</html>
